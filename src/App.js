834fc4ca195272bc3090aa78adef5330168872fa
Samuel Ševčik <s.sevcik.st@spseiostrava.cz>
2021-05-25 15:07:32 +0200

    added few components, integrated commerce.js with this app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { commerce } from './lib/commerce';
 
 // import Products from './components/Products/Products';
 // import Navbar from './components/Navbar/Navbar';
 
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -15,17 +17,36 @@ const App = () => {
     setProducts(data);
   }
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  }
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  }
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
 
-  console.log(products);
-
+  console.log(cart)
   return (
-    <div>
-      <Navbar />
-      <Products />
-    </div>
+    <Router>
+      <div>
+        <Navbar totalItems={cart.total_items}/>
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart}/>
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,12 +4,7 @@ import {Grid} from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-  { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', },
-  { id: 2, name: 'Macbook', description: 'Apple Macbook.', price: '$10', image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.alza.cz%2Fmacbook-air-13-retina-2018%3Fdq%3D5799270&psig=AOvVaw2Dulyeptspeo-_nz_atZbG&ust=1622018336312000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCJC4s6i35PACFQAAAAAdAAAAABAO'},
-];
-
-const Products = () => {
+const Products = ({products, onAddToCart}) => {
   const classes = useStyles();
 
   return(
@@ -18,7 +13,7 @@ const Products = () => {
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product product={product}/>
+            <Product product={product} onAddToCart={onAddToCart}/>
           </Grid>
         ))}
       </Grid>
